f718c5a0314b48da431d86176197e2d2f3765ed0
Lee Cooper <cooperlee372@gmail.com>
2021-09-16 14:37:40 -0400

    add mobile header to site, remove gif, fix accessibility issues

--- scripts.js
+++ scripts.js
@@ -2,9 +2,11 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const profile = document.getElementsByClassName("profile")[0];
 const widgets = document.getElementsByClassName("widgets")[0];
 const sidebar = document.getElementsByClassName("sidebar")[0];
+const mobileHeader = document.getElementsByClassName("mobile-header")[0];
 
 toggleButton.addEventListener("click", () => {
   profile.classList.toggle("active");
   widgets.classList.toggle("active");
   sidebar.classList.toggle("active");
+  mobileHeader.classList.toggle("active");
 });
