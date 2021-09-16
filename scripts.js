2af5a4d7ec0b2c9008e6da0b135fad461a0ba873
Lee Cooper <cooperlee372@gmail.com>
2021-09-15 22:18:06 -0400

    fix css styling

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const profile = document.getElementsByClassName("profile")[0];
+const widgets = document.getElementsByClassName("widgets")[0];
+const sidebar = document.getElementsByClassName("sidebar")[0];
+
+toggleButton.addEventListener("click", () => {
+  profile.classList.toggle("active");
+  widgets.classList.toggle("active");
+  sidebar.classList.toggle("active");
+});
