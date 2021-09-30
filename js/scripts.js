c0e8de0d05fbc2a10fa906435843053fc00eea3d
Lee Cooper <cooperlee372@gmail.com>
2021-09-29 23:08:21 -0400

    change sidebar behavior

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,9 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const profile = document.getElementsByClassName("profile")[0];
 const widgets = document.getElementsByClassName("widgets")[0];
 const sidebar = document.getElementsByClassName("sidebar")[0];
-const mobileHeader = document.getElementsByClassName("mobile-header")[0];
 
 toggleButton.addEventListener("click", () => {
   profile.classList.toggle("active");
   widgets.classList.toggle("active");
   sidebar.classList.toggle("active");
-  mobileHeader.classList.toggle("active");
 });
