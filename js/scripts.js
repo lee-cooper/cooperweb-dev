4a7ecb03b93ff80e2f6b5c3bcc6df0e9f78fc2ec
Lee Cooper <cooperlee372@gmail.com>
2021-10-18 16:19:28 -0400

    refactor website to simplify design, become more responsive + maintainable

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const profile = document.getElementsByClassName("profile")[0];
-const widgets = document.getElementsByClassName("widgets")[0];
+const menuToggle = document.getElementsByClassName("sidebar-toggle")[0];
 const sidebar = document.getElementsByClassName("sidebar")[0];
 
 toggleButton.addEventListener("click", () => {
-  profile.classList.toggle("active");
-  widgets.classList.toggle("active");
+  sidebar.classList.toggle("active");
+  toggleButton.classList.toggle("active");
+});
+
+menuToggle.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   toggleButton.classList.toggle("active");
 });
