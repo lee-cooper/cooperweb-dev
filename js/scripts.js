22551f66a831282ba325d4d9bb80e82ff6a1eefc
Lee Cooper <cooperlee372@gmail.com>
2021-10-18 22:59:28 -0400

    hide open sidebar button on click

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,10 @@ const sidebar = document.getElementsByClassName("sidebar")[0];
 
 openSidebarButton.addEventListener("click", () => {
   sidebar.classList.toggle("active");
+  openSidebarButton.classList.toggle("hide");
 });
 
 closeSidebarButton.addEventListener("click", () => {
   sidebar.classList.toggle("active");
+  openSidebarButton.classList.toggle("hide");
 });
