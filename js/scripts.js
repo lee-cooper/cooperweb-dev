fbda702a00e8beb956f4ad05e093d1e55f7a1e54
Lee Cooper <cooperlee372@gmail.com>
2021-10-23 13:08:54 -0400

    css-only sidebar trigger, remove need for javascript to open site menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,16 @@
 'use strict';
 
-// Sidebar buttons
 const openSidebarButton = document.getElementById("open-sidebar-button");
 const closeSidebarButton = document.getElementById("close-sidebar-button");
-const sidebar = document.getElementsByClassName("sidebar")[0];
-const mainElement = document.getElementsByTagName("main")[0];
 
 openSidebarButton.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-
   openSidebarButton.classList.toggle("disabled");
   openSidebarButton.disabled = true;
   closeSidebarButton.focus();
-
-  // mainElement.classList.toggle("dim");
 });
 
 closeSidebarButton.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-
   openSidebarButton.classList.toggle("disabled");
   openSidebarButton.disabled = false;
   openSidebarButton.focus();
-
-  // mainElement.classList.toggle("dim");
 });
