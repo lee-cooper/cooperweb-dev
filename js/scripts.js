4fb891ea981b7901e6920faf83d8c6a886725639
Lee Cooper <cooperlee372@gmail.com>
2021-11-13 13:54:06 -0500

    refactor theme javascript code - event delegation

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,57 @@
 const root = document.documentElement;
 
 document.addEventListener("DOMContentLoaded", () => {
+  setupSiteTheme();
+});
+
+function getTheme() {
+  return {
+    primaryText: localStorage.getItem("--primary-text-color"),
+    primaryBackground: localStorage.getItem("--primary-background-color"),
+    secondaryText: localStorage.getItem("--secondary-text-color"),
+    secondaryBackground: localStorage.getItem("--secondary-background-color"),
+  };
+}
+
+function setTheme(
+  primaryText,
+  primaryBackground,
+  secondaryText,
+  secondaryBackground
+) {
+  localStorage.setItem("themeIsSet", true);
+  localStorage.setItem("--primary-text-color", primaryText);
+  localStorage.setItem("--primary-background-color", primaryBackground);
+  localStorage.setItem("--secondary-text-color", secondaryText);
+  localStorage.setItem("--secondary-background-color", secondaryBackground);
+
+  root.style.setProperty("--primary-text-color", primaryText);
+  root.style.setProperty("--primary-background-color", primaryBackground);
+  root.style.setProperty("--secondary-text-color", secondaryText);
+  root.style.setProperty("--secondary-background-color", secondaryBackground);
+}
+
+function setupSiteTheme() {
   const themeIsSet = localStorage.getItem("themeIsSet");
 
-  if(themeIsSet === null) {
+  if (!themeIsSet) {
     setTheme("#000", "#f8f8f8", "#f8f8f8", "#000");
+    return;
   }
 
-  const primaryText = localStorage.getItem("--primary-text-color");
-  const primaryBackground = localStorage.getItem("--primary-background-color");
-  const secondaryText = localStorage.getItem("--secondary-text-color");
-  const secondaryBackground = localStorage.getItem("--secondary-background-color");
-
+  const { primaryText, primaryBackground, secondaryText, secondaryBackground } =
+    getTheme();
   setTheme(primaryText, primaryBackground, secondaryText, secondaryBackground);
+}
+
+const themes = document.getElementById("themes");
+themes.addEventListener("click", (event) => {
+  if (event.target.id === "bubblegum")
+    setTheme("#000", "#fff", "#000", "#fce2ff");
+  if (event.target.id === "oreo")
+    setTheme("#000", "#f8f8f8", "#f8f8f8", "#000");
+  if (event.target.id === "electric-lemonade")
+    setTheme("#000", "#f8f8f8", "#f8f8f8", "#0063B2");
 });
 
 const openSidebarButton = document.getElementById("open-sidebar-button");
@@ -30,34 +69,4 @@ closeSidebarButton.addEventListener("click", () => {
   openSidebarButton.classList.toggle("disabled");
   openSidebarButton.disabled = false;
   openSidebarButton.focus();
-});
-
-function setTheme(primaryText, primaryBackground, secondaryText, secondaryBackground) {
-  localStorage.setItem("themeIsSet", true);
-  localStorage.setItem("--primary-text-color", primaryText);
-  localStorage.setItem("--primary-background-color", primaryBackground);
-  localStorage.setItem("--secondary-text-color", secondaryText);
-  localStorage.setItem("--secondary-background-color", secondaryBackground);
-
-  root.style.setProperty("--primary-text-color", primaryText);
-  root.style.setProperty("--primary-background-color", primaryBackground);
-  root.style.setProperty("--secondary-text-color", secondaryText);
-  root.style.setProperty("--secondary-background-color", secondaryBackground);
-}
-
-const bubblegumButton = document.getElementById("bubblegum");
-const oreoButton = document.getElementById("oreo");
-const electricLemonadeButton = document.getElementById("electric-lemonade");
-
-bubblegumButton.addEventListener("click", () => {
-  setTheme("#000", "#fff", "#000", "#fce2ff");
-});
-
-oreoButton.addEventListener("click", () => {
-  setTheme("#000", "#f8f8f8", "#f8f8f8", "#000");
-});
-
-electricLemonadeButton.addEventListener("click", () => {
-  setTheme("#000", "#f8f8f8", "#f8f8f8", "#0063B2");
-  //#fdff54 yellow
 });
