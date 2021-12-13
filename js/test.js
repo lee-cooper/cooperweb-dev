41747b990621602d2445be85ae9aed434ff99437
Lee Cooper <cooperlee372@gmail.com>
2021-12-13 18:10:53 -0500

    javascript console article updates

---
+++ js/test.js
@@ -0,0 +1,16 @@
+// How to use console Log
+
+// Logging objects in console log
+
+// logging with a message and an object
+
+// console.log('This can be anything', { hello: 'Greetings' }, 'hi');
+const person = {
+  name: 'Bob',
+};
+
+console.log(person);
+
+person.name = 'Jim';
+
+console.log(person);
