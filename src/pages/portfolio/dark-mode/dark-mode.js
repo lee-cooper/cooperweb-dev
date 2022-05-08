5dfb5c63b1b404b89b4997ae6b15c851b5702bce
Lee Cooper <cooperlee372@gmail.com>
2022-05-08 17:43:09 -0400

    progress on dark mode article

--- src/pages/portfolio/dark-mode/dark-mode.js
+++ src/pages/portfolio/dark-mode/dark-mode.js
@@ -4,14 +4,4 @@ const switchModeButton = document.querySelector('#switch-mode');
 
 switchModeButton.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-mode');
-
-  // document.documentElement.style.setProperty(
-  //   '--primary-background-color',
-  //   '#2f2f2f'
-  // );
-  // document.documentElement.style.setProperty('--primary-text-color', '#f0f0f0');
-  // document.documentElement.style.setProperty(
-  //   '--secondary-background-color',
-  //   '#575757'
-  // );
 });
